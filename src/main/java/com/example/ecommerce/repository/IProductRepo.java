64336954a26b00e010a3df692070b62c3934cdae
NguyenMinhJuan <nmtuan.dhti14a4hn@sv.uneti.edu.vn>
2024-12-17 14:54:53 +0700

    tuan lam back end load product by category

--- src/main/java/com/example/ecommerce/repository/IProductRepo.java
+++ src/main/java/com/example/ecommerce/repository/IProductRepo.java
@@ -1,9 +1,11 @@
 package com.example.ecommerce.repository;
 
+import com.example.ecommerce.model.Category;
 import com.example.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IProductRepo extends JpaRepository<Product, Long> {
+    Iterable<Product> findAllByCategory(Category category);
 }
